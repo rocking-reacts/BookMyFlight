b3b3a30e00c35123fc8d84dcf6c237ec2a6395a7
ibmjavafsdmr17 <ibmjavafsdmr17@host.docker.internal>
2021-04-24 22:25:18 +0530

    Passenger entity and repo

---
+++ src/main/java/com/bookmyflight/entity/Passenger.java
@@ -0,0 +1,58 @@
+package com.bookmyflight.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Shivani Jadon
+ * Passenger entity stores passenger information
+ * Like passenger id, name, gender and age.
+ */
+@Entity
+@Table(name = "passenger")
+public class Passenger {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "pass_id")
+	private int pid;
+	@Column(name = "pass_name")
+	private String pname;
+	private String gender;
+	private int age;
+	
+	public Passenger() {
+	}
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
+
